4c9499b811968a8918c44dbd6da5373c2034e478
Italo Oliveira <italooliveira-dev@hotmail.com>
2025-05-17 20:38:26 -0300

    commit inicial

---
+++ src/main/java/br/com/italooliveira/appagendamento/AppAgendamentoApplication.java
@@ -0,0 +1,13 @@
+package br.com.italooliveira.appagendamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppAgendamentoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppAgendamentoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/italooliveira/appagendamento/AppAgendamentoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.italooliveira.appagendamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppAgendamentoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
